3caffdf3418c982b6ac44e5ed632ad8fb7788d24
Julien Pinard <julien.pinard@epitech.eu>
2023-11-15 21:19:20 +0100

    new push

---
+++ main.py
@@ -0,0 +1,37 @@
+# This example requires the 'message_content' intent.
+
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        channel = self.get_channel(message.channel.id)
+        print(message.author)
+        new_content = message.content
+        if "https://twitter.com" in message.content:
+            new_content = message.content.replace("twitter.com", "vxtwitter.com")
+            await channel.send(new_content)
+
+        if "https://x.com" in message.content:
+            new_content = message.content.replace("x.com", "vxtwitter.com")
+            await channel.send(new_content)
+
+        print(f'Message from {message.author}: {new_content}')
+
+
+intents = discord.Intents.default()
+
+client = MyClient(intents=intents)
+token = os.getenv("DISCORD_TOKEN")
+
+client.run(token)
